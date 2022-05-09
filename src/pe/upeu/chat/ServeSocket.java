4abc83b115129f97db6ad108092bffa3fbd79585
sarita-d <sholey@upeu.edu.pe>
2022-05-08 20:18:28 -0500

    Mi primer actualizacion

---
+++ src/pe/upeu/chat/ServeSocket.java
@@ -0,0 +1,40 @@
+package pe.upeu.chat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServeSocket {
+
+    public static void main(String[] args) throws IOException {
+        String input, output;
+        
+        ServerSocket serverSocket = new ServerSocket(6789);
+        
+        Socket connectionSocket = serverSocket.accept();
+        
+        Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
+        PrintWriter outFromServer = new PrintWriter(connectionSocket.getOutputStream(),true);
+        Scanner inFromServer = new Scanner(System.in);
+        
+        while (true) {
+            input = inFromClient.nextLine();
+            System.out.println("Client: "+input);
+            
+            if(input.equals("**close**")){
+                break;
+            }
+            
+            System.out.print("Server: ");
+            output = inFromServer.nextLine();
+
+            outFromServer.println(output);
+           
+            //outFromServer.flush();
+            
+        }
+        serverSocket.close();
+    }
+}
